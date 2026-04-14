b2886ddfee9411583446469970f5283bfd386cd2
OhChangEun <dwc07109@gmail.com>
2026-04-14 15:21:26 +0900

    refac: MrpTab 리팩토링

--- src/app/(private)/production/components/tabs/MrpTab.tsx
+++ src/app/(private)/production/components/tabs/MrpTab.tsx
@@ -1,12 +1,36 @@
 'use client';
 
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { MRP_TABS } from '@/app/(private)/production/constants';
-import SubNavigation from '@/app/components/common/SubNavigation';
+import TabButtons from '@/app/components/common/TabButtons';
 
 export default function MrpTab() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+
+  const currentSubTab = searchParams.get('subTab') || MRP_TABS[0]?.id;
+
+  const handleSubTabChange = (tabId: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set('subTab', tabId);
+    router.replace(`${pathName}?${params}`);
+  };
+
+  const activeTab = MRP_TABS.find((tab) => tab.id === currentSubTab);
+  const ActiveComponent = activeTab?.component;
+
+  const tabButtons = (
+    <TabButtons
+      tabs={MRP_TABS.map(({ id, name, icon }) => ({ id, name, icon }))}
+      activeTab={currentSubTab}
+      onTabChange={handleSubTabChange}
+    />
+  );
+
   return (
-    <>
-      <SubNavigation tabs={MRP_TABS} />
-    </>
+    <div className="flex flex-col h-full">
+      {ActiveComponent && <ActiveComponent tabButtons={tabButtons} />}
+    </div>
   );
 }
